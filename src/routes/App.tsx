fc0cddb9591920c42a003857b1d673ed57c085d6
Nihil <mukkeshnihil@gmail.com>
2026-03-04 16:27:58 +0530

    added app.tsx

---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+// 1. Update the import to grab AnyRouter instead of Router
+import { RouterProvider, type AnyRouter } from "@tanstack/react-router";
+
+// 2. Update your interface to use AnyRouter
+interface AppProps {
+	router: AnyRouter;
+}
+
+export default function App({ router }: AppProps) {
+	return <RouterProvider router={router} />;
+}
+
